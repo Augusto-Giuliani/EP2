062cd84c84240701c810c223ad145976a8552825
Augusto <augustomg@al.insper.edu.br>
2021-10-23 16:38:44 -0300

    Criando arquivo que terá as funções obrigatórias

---
+++ mandatory_functions.py
@@ -0,0 +1,13 @@
+#Implementando as funções obrigatórias (oriundas dos exercícios na Academia Python)
+
+# CRIANDO PEÇAS DE DOMINÓ:
+
+#INICIANDO O JOGO DO DOMINÓ:
+
+#QUEM GANHOU NO DOMINÓ?:
+
+#SOMA PEÇAS DO DOMINÓ:
+
+#POSIÇÕES POSSÍVEIS DA MÃO:
+
+#ADICIONANDO PEÇAS A MESA NUM JOGO DE DOMINÓ:
