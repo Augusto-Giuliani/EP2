0b65e47deaadb37f9661ae7d23a0fd9661de01a0
Augusto <augustomg@al.insper.edu.br>
2021-10-30 12:00:02 -0300

    Importando as funções obrigatórias e Introdução

---
+++ game.py
@@ -0,0 +1,7 @@
+# Implementando o código central do jogo de dominó
+
+# Importando as funções obrigatórias
+from mandatory_functions import cria_pecas,inicia_jogo,verifica_ganhador,soma_pecas,posicoes_possiveis,adiciona_na_mesa
+
+# Introdução
+print('INSPER - Curso de Engenharia - 1° Semestre - 2021.2\nDisciplina: Design de Software\nProfessor: Humberto Sandmann\nAluno: Augusto Giuliani\nAtividade: Exercício Programa 2 - Jogo de Dominó')
