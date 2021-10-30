cba5847b9148089d66b816cd0981c9edaf4cd3a2
Augusto <augustomg@al.insper.edu.br>
2021-10-30 14:37:04 -0300

    Iniciando a Rodada - criando outro loop

--- game.py
+++ game.py
@@ -22,10 +22,26 @@ while GAME:
 # Criando a lista de peças e fazendo seu devido embaralhamento
     l_pieces = cria_pecas()
 # Distribuindo 7 peças para cada jogador, criando monte e mesa (retorna dicionário com tudo isso).
-# NOTE: O usuário é o jogador "0" desse dicionário
     d_game = inicia_jogo(n,l_pieces)
+# Quebrando o dicionário em diferentes variáveis
+    players = d_game['jogadores'] # --> NOTE: O usuário é o jogador "0" desse dicionário
+    table = d_game['mesa']
+    storage = d_game['monte']
 # Um dos jogadores é aleatoriamente escolhido para iniciar a rodada
     player = r.randint(0,n-1)
+    # INICIANDO A RODADA - CRIANDO OUTRO LOOP
+    ROUND = True
+    while ROUND:
+    # Mostrando as peças sobre a mesa e as peças do jogador
+        
+    # Verificando as peças possíveis (que podem ser colocadas na mesa) do jogador
+        possibilities = posicoes_possiveis(table,players[player])
+    # Se o jogador não tiver peças "possíveis", ele pega do monte até ter uma peça possível.
+        if possibilities == list() and storage!=list():
+        # Se o jogador for o usuário
+            if player==0:
+                sad_message = input('Eita! Você vai ter que pegar uma peça do monte. Aperte Enter para apanhar uma peça do monte.')
+
     
          
 
